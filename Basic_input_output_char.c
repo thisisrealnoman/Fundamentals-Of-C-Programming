d4907e6410dfa47fcab2c83eaf8d7c66df316ea6
Abdullah Al Noman <136128362+thisisrealnoman@users.noreply.github.com>
2023-06-10 23:47:07 +0600

    Basic_input_output_char.c

--- Basic_input_output_char.c
+++ Basic_input_output_char.c
@@ -1,20 +1,3 @@
-/*#include <stdio.h>
-
-int main(){
-
-    char ch;
-
-    printf("Enter Your Grade: ");
-    scanf("%c", &ch);
-
-    printf("Your Grade is: %c", ch);
-
-    return 0;
-}
-*/
-/* Please work */
-
-
 #include <stdio.h>
 
 int main(){
