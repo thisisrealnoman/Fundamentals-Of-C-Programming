28377c8a9f888a5cfdfc3f62b34f73ecc9a5c3dc
Abdullah Al Noman <136128362+thisisrealnoman@users.noreply.github.com>
2023-06-10 23:50:43 +0600

    Add files via upload

---
+++ Basic_input_output_sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, sum;
+
+    printf("Enter the value of Num 1: ");
+    scanf("%d", &num1);
+
+    printf("Enter the value of Num 2: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+
+}
